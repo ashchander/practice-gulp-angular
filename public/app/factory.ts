1d90bd683ff24bde3a2c1b7773e1ee14e396abd4
Ashwin Chander <ashchander@gmail.com>
2015-08-07 15:57:50 -0400

    Implemented factory

--- public/app/factory.ts
+++ public/app/factory.ts
@@ -1,5 +1,30 @@
+myApp.factory('MainFactory', ['$http', '$q', ($http: ng.IHttpService, $q: ng.IQService) => new MainFeature.MainFactory($http, $q)]);
+
 module MainFeature {
+  interface FactoryError {
+      message: string;
+  }
+
   export class MainFactory {
-    
+    private baseUrl: string;
+    private http: ng.IHttpService;
+    private q: ng.IQService;
+
+    constructor($http: ng.IHttpService, $q: ng.IQService) {
+      this.baseUrl = '/api';
+      this.http = $http;
+    }
+
+    public getExistingPersons(): ng.IPromise<Array<string>>{
+      var defer = this.q.defer();
+      this.http.get(this.baseUrl).
+        then(function(response: Array<string>) {
+          defer.resolve(response);
+        }, function(response: FactoryError) {
+          defer.reject(response);
+        }
+      );
+      return defer.promise;
+    }
   }
 }
